b60bdac5db1909086611f72a6f6250b9a7ee8775
David <david_halim90@yahoo.com>
2024-10-10 22:59:08 +0800

    Initialise account

--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -18,5 +18,12 @@ void test_validate_my_username()
      * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
      * config file and my_username() functions are setup properly
      */
-    TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
+    // TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
+    const char *username = my_username();
+    const char *conf_username = malloc_username_from_conf_file();
+
+    char message[200];
+    sprintf(message, "Username %s and config username %s are not equal", username, conf_username);
+
+    TEST_ASSERT_EQUAL_STRING_MESSAGE(username, conf_username, message);
 }
